8f1f00a4557a6fc564b6157a6322dde42780371e
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2024-03-15 11:24:52 +0100

    base graphique

---
+++ lib/view/MyHomePage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:tpnote/view/pages/categories/cartegoriesPage.dart';
+import 'package:tpnote/view/pages/detail/detailPage.dart';
+import 'package:tpnote/view/pages/home/homePage.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+    int selectedIndex = 0;
+    
+    void setSelectedIndex(int index) {
+      setState(() {
+        selectedIndex = index;
+        print(selectedIndex);
+      });
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: [
+        const homePage(),
+        const detailPage(),
+        const categoriesPage()
+      ][selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) => setSelectedIndex(index),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list, color: Colors.black),
+            label: 'Detail',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category, color: Colors.black),
+            label: 'Categories',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
+ 
